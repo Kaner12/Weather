df86af6d308c63bd54c50c79c03a0600788a8c3f
Kaner12 <sgushchenkad@bk.ru>
2024-02-06 12:58:14 +0500

    Little changes of code and style

--- show.js
+++ show.js
@@ -3,14 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
     function showMoreInfo(button) {
       var card = button.closest('.card-container');
       var moreInfo = card.querySelector('.more-info');
+
       card.classList.toggle('show-more-info');
       moreInfo.classList.toggle('show');
-
       button.classList.toggle('expanded');  
 
-      var buttonText = button.textContent.trim();
-
-      button.textContent = buttonText === 'Подробнее' ? 'Скрыть' : 'Подробнее';
+      button.textContent =button.textContent.trim() === 'Подробнее' ? 'Скрыть' : 'Подробнее';
     }
     
     window.showMoreInfo = showMoreInfo;
